909a8c06050f0eb5ec5ff5ad2f4ee18bc9ac4981
mijinko17 <kougouseikeidansi@gmail.com>
2021-04-12 01:28:05 +0900

    Add temporary code.

--- typesafe_derive_builder/src/tests.rs
+++ typesafe_derive_builder/src/tests.rs
@@ -5,8 +5,21 @@ use crate::Builder;
 fn emit_no_error() {
     #[derive(Builder)]
     #[allow(dead_code)]
-    struct Hoge {
+    pub struct Person {
         name: String,
         age: u32,
+        address: Option<String>,
     }
+}
+
+#[test]
+fn call_builder() {
+    #[allow(dead_code)]
+    #[derive(Builder)]
+    pub struct Person {
+        name: String,
+        age: u32,
+        address: Option<String>,
+    }
+    let _ = Person::builder();
 }
--- typesafe_derive_builder_core/src/lib.rs
+++ typesafe_derive_builder_core/src/lib.rs
@@ -2,7 +2,52 @@ use quote::quote;
 use syn::DeriveInput;
 
 pub fn builder_token_stream(_: DeriveInput) -> proc_macro2::TokenStream {
-    quote! {}
+    quote! {
+        impl Person {
+            pub fn builder() -> PersonBuilder<(), ()> {
+                PersonBuilder {
+                    name: (),
+                    age: (),
+                    address: None,
+                }
+            }
+        }
+
+        pub struct PersonBuilder<Name, Age> {
+            name: Name,
+            age: Age,
+            address: Option<String>,
+        }
+        impl<Name, Age> PersonBuilder<Name, Age> {
+            pub fn name(self, name: &str) -> PersonBuilder<String, Age> {
+                PersonBuilder {
+                    name: name.to_string(),
+                    age: self.age,
+                    address: self.address,
+                }
+            }
+            pub fn age(self, age: u32) -> PersonBuilder<Name, u32> {
+                PersonBuilder {
+                    name: self.name,
+                    age: age,
+                    address: self.address,
+                }
+            }
+            pub fn address(mut self, address: &str) -> PersonBuilder<Name, Age> {
+                self.address = Some(address.to_string());
+                self
+            }
+        }
+        impl PersonBuilder<String, u32> {
+            pub fn build(self) -> Person {
+                Person {
+                    name: self.name,
+                    age: self.age,
+                    address: self.address,
+                }
+            }
+        }
+    }
 }
 
 #[cfg(test)]
